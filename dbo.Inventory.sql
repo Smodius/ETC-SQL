4f46d8e5a1fadf56d377a0be562500ba39695698
Smodius <136461413+Smodius@users.noreply.github.com>
2023-06-17 00:47:50 +0800

    Add files via upload
    Database for etc inventory system

---
+++ dbo.Inventory.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Inventory] (
+    [Id]            INT            IDENTITY (1, 1) NOT NULL,
+    [Item]          NVARCHAR (MAX) NULL,
+    [Name]          NVARCHAR (MAX) NULL,
+    [Date_Borrowed] NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Inventory] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
